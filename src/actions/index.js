8351ec42d413ec3f71dea8660f3d98a9d08632d5
alan-maldonado <alan.maldonado@wizeline.com>
2016-08-04 15:45:24 -0500

    project weather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '40a6e89d23aad0a8a229b4b1f186512b';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const URL = `${ROOT_URL}&q=${city},mx`;
+  const request = axios.get(URL);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
